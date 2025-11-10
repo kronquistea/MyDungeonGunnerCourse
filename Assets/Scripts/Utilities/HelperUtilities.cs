93c913cc03400a4b6d5734f747402b47247e0ba6
Erik Kronquist <erik.kronquist@gmail.com>
2025-11-09 22:18:04 -0700

    Add basic attributes for room node graphs and room nodes - L9

---
+++ Assets/Scripts/Utilities/HelperUtilities.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class HelperUtilities
+{
+    /// <summary>
+    /// string empty check - returns true is string is empty
+    /// </summary>
+    /// <param name="thisObject"></param>
+    /// <param name="fieldName"></param>
+    /// <param name="stringToCheck"></param>
+    /// <returns>bool true/false</returns>
+    public static bool ValidateCheckEmptyString(Object thisObject, string fieldName, string stringToCheck)
+    {
+        if (stringToCheck == "")
+        {
+            Debug.Log(fieldName + " is empty and must contain a vlaue in object " + thisObject.name.ToString());
+            return true;
+        }
+        return false;
+    }
+
+    /// <summary>
+    /// list empty or contains null value check - returns true is there is an error
+    /// </summary>
+    /// <param name="thisObject"></param>
+    /// <param name="fieldName"></param>
+    /// <param name="enumerableObjectToCheck"></param>
+    /// <returns>bool error</returns>
+    public static bool ValidateCheckEnumerableValues(Object thisObject, string fieldName, IEnumerable enumerableObjectToCheck)
+    {
+        bool error = false;
+        int count = 0;
+
+        foreach (var item in enumerableObjectToCheck)
+        {
+            if (item == null)
+            {
+                Debug.Log(fieldName + " has null values in object " + thisObject.name.ToString());
+                error = true;
+            }
+            else
+            {
+                count++;
+            }
+        }
+
+        if (count == 0)
+        {
+            Debug.Log(fieldName + " has no values in object " + thisObject.name.ToString());
+            error = true;
+        }
+
+        return error;
+    }
+}
