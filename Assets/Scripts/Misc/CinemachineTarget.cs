6e0d2760dc62701d853c066b6d3e2663a25f4de7
Erik Kronquist <erik.kronquist@gmail.com>
2026-03-22 10:54:47 -0600

    Set cinemachine to follow player and screen cursor

--- Assets/Scripts/Misc/CinemachineTarget.cs
+++ Assets/Scripts/Misc/CinemachineTarget.cs
@@ -9,6 +9,11 @@ public class CinemachineTarget : MonoBehaviour
 {
     private CinemachineTargetGroup cinemachineTargetGroup;
 
+    #region Tooltip
+    [Tooltip("Populate with the CursorTarget gameobject")]
+    #endregion
+    [SerializeField] private Transform cursorTarget;
+
     private void Awake()
     {
         // Load components
@@ -28,22 +33,35 @@ public class CinemachineTarget : MonoBehaviour
     /// </summary>
     private void SetCinemachineTargetGroup()
     {
-        // Create target group for cinemachine for the cinemachine camera to follow
+        // Create target group for cinemachine for the cinemachine camera to follow - group will include the player and screen cursour
         CinemachineTargetGroup.Target cinemachineGroupTarget_player = new CinemachineTargetGroup.Target
         {
             weight = 1f,
-            radius = 1f,
+            radius = 2.5f,
             target = GameManager.Instance.GetPlayer().transform
+        };
+
+        CinemachineTargetGroup.Target cinemachineGroupTarget_cursor = new CinemachineTargetGroup.Target
+        {
+            weight = 1f,
+            radius = 1f,
+            target = cursorTarget
         };
 
         // Array to hold all cinemachine target group targets
         // Cinemachine will follow both the player and the aiming cursor, that is the point of the array
         CinemachineTargetGroup.Target[] cinemachineTargetArray = new CinemachineTargetGroup.Target[]
         {
-            cinemachineGroupTarget_player
+            cinemachineGroupTarget_player,
+            cinemachineGroupTarget_cursor
         };
 
         // m_Targets is the same as the list of targets for the CinemachineTargetGroup in Unity
         cinemachineTargetGroup.m_Targets = cinemachineTargetArray;
+    }
+
+    private void Update()
+    {
+        cursorTarget.position = HelperUtilities.GetMouseWorldPosition();
     }
 }
